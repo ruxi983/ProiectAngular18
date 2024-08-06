3c7c7670691ebfb7e449eba774193f04225f4c7f
ruxi983 <ruxandramartoiu@gmail.com>
2024-08-06 14:44:31 +0300

    reactive forms added, I have to fix the confirm password error message issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import {provideHttpClient} from '@angular/common/http';
 import {Routes, RouterModule} from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,12 +13,16 @@ import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 import { PageNotFoundComponent } from './property/page-not-found/page-not-found.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
 
 const appRoutes: Routes = [
   {path: '', component: PropertyListComponent},
   {path: 'rent-property', component: PropertyListComponent},
   {path: 'add-property', component: AddPropertyComponent},
   {path: 'property-detail/:id', component: PropertyDetailComponent},
+  {path: 'user/login', component: UserLoginComponent},
+  {path: 'user/register', component: UserRegisterComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 
@@ -30,13 +34,16 @@ const appRoutes: Routes = [
     NavBarComponent,
     AddPropertyComponent,
     PropertyDetailComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    UserRegisterComponent,
+    UserLoginComponent
 
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
---
+++ src/app/user/user-register/user-register.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { format } from 'path';
+
+@Component({
+  selector: 'app-user-register',
+  templateUrl: './user-register.component.html',
+  styleUrls: ['./user-register.component.css']
+})
+export class UserRegisterComponent implements OnInit {
+
+registrationForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+      this.registrationForm=new FormGroup({
+        userName: new FormControl(null,Validators.required),
+        email: new FormControl(null,[Validators.required,Validators.email]),
+        password: new FormControl(null,[Validators.required, Validators.minLength(8)]),
+        confirmPassword: new FormControl(null,[Validators.required]),
+        mobile: new FormControl(null,[Validators.required, Validators.maxLength(10)])
+
+
+      }, this.passwordMatchingValidator);
+    }
+
+    //  passwordMatchingValidator(fc: AbstractControl): ValidationErrors | null{
+    //    return fc.get('password')?.value === fc.get('confirmPassword')?.value ? null:
+    //    {notmatched: true};
+    //    console.log(this.registrationForm);
+    //  }
+
+     passwordMatchingValidator: ValidatorFn = (
+      control: AbstractControl
+    ): ValidationErrors | null => {
+      return control.value.password === control.value.confirmPassword
+        ? null
+        : { notmatched: true };
+    };
+
+
+    //getter method for all form controls
+    get userName(){
+        return this.registrationForm.get('userName') as FormControl;
+    }
+
+    get email(){
+        return this.registrationForm.get('email') as FormControl;
+    }
+
+    get password(){
+        return this.registrationForm.get('password') as FormControl;
+    }
+
+    get confirmPassword(){
+        return this.registrationForm.get('confirmPassword') as FormControl;
+    }
+
+    get mobile(){
+        return this.registrationForm.get('mobile') as FormControl;
+    }
+
+  
+
+  onSubmit(){
+    console.log(this.registrationForm);
+  }
+
+
+}
