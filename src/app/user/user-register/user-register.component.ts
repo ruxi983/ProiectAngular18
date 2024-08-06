cfa7ba684cbf21ff7feed5d01dab9933be3db380
ruxi983 <ruxandramartoiu@gmail.com>
2024-08-06 15:20:24 +0300

    added default answers for the reactive form and solved the isue withe the password matching error

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -20,24 +20,27 @@ registrationForm: FormGroup;
         password: new FormControl(null,[Validators.required, Validators.minLength(8)]),
         confirmPassword: new FormControl(null,[Validators.required]),
         mobile: new FormControl(null,[Validators.required, Validators.maxLength(10)])
-
-
       }, this.passwordMatchingValidator);
+
+          this.registrationForm.controls['userName'].setValue('e.g: McConor John'),
+          this.registrationForm.controls['mobile'].setValue('e.g: 0712345678'),
+          this.registrationForm.controls['email'].setValue('e.g: mcconorjohn@business.uk')
+
     }
 
-    //  passwordMatchingValidator(fc: AbstractControl): ValidationErrors | null{
-    //    return fc.get('password')?.value === fc.get('confirmPassword')?.value ? null:
-    //    {notmatched: true};
-    //    console.log(this.registrationForm);
-    //  }
+      passwordMatchingValidator(fc: AbstractControl): ValidationErrors | null{
+        return fc.get('password')?.value === fc.get('confirmPassword')?.value ? null:
+        {notmatched: true};
+        console.log(this.registrationForm);
+      }
 
-     passwordMatchingValidator: ValidatorFn = (
-      control: AbstractControl
-    ): ValidationErrors | null => {
-      return control.value.password === control.value.confirmPassword
-        ? null
-        : { notmatched: true };
-    };
+    //  passwordMatchingValidator: ValidatorFn = (
+    //   control: AbstractControl
+    // ): ValidationErrors | null => {
+    //   return control.value.password === control.value.confirmPassword
+    //     ? null
+    //     : { notmatched: true };
+    // };
 
 
     //getter method for all form controls
